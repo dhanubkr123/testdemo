2ac61daea82a20aed694fa68258dd6d8159c054f
choug <choug@LAPTOP-354T6290>
2021-08-09 16:24:32 +0530

    change

---
+++ try/src/testtry/testtry.java
@@ -0,0 +1,35 @@
+package testtry;
+
+public class testtry {
+	public static void main(String [] args)
+	{
+	int a=10;
+	int b=2;
+	int []c = {1,5,9};
+	
+	try
+	{
+		int d =a/b;
+		System.out.println(d);
+		System.out.println(c[9]);
+	}
+	
+	
+	
+	catch(ArithmeticException e)
+	{
+		System.out.println("b=0 change the value");
+	}
+	
+	catch(ArrayIndexOutOfBoundsException r)
+	{
+		System.out.println(c[2]);
+	}
+	
+	catch(Exception x)
+
+	{
+		System.out.println("last catch");
+	}
+}
+}
